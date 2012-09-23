77871172531bace118a99ed21fdc7dad44d5b041
Abu Zahed Jony <jony.cse@hotmail.com>
2012-09-24 05:10:47 +0600

    chapter 05 complete
    GData.objects.get(id=g_id).values('person','address')

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.forms import forms
 
-# Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+    publisher_description = models.CharField(max_length=200, default=" This publisher description") # set editable=False
+    #publisher_description = models.CharField(max_length=200, default=" This publisher description", editable=False)
+
+
+    class Meta(object):
+        ordering  = ['name', 'city']
+        verbose_name_plural ='Publisher Table'
+
+    def __unicode__(self):
+        return self.name
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    publication_date = models.DateField()
+
+    def __unicode__(self):
+        return self.title
