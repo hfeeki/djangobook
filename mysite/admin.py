5366e3befe9d04f296e5f8ff8907d4e502290c0a
Abu Zahed Jony <jony.cse@hotmail.com>
2012-09-25 01:32:14 +0600

    chapter 06 complete
    some cool admin feature added

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from mysite.models import Publisher, Author, Book
 
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date',)
+    filter_horizontal = ('authors',)
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)
+    fields = ('title', 'authors', 'publisher', 'publication_date')
+    raw_id_fields = ('publisher',)
+    #fields = ('title', 'authors', 'publisher') # will hide publication_date field
+
 admin.site.register(Publisher)
-admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
